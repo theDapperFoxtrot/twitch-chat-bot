8dbac5bbed0104908a2fb89491dde40fa7b96cc8
Viktor Hrytsai <vhrytsai@atlassian.com>
2022-09-08 19:38:26 +0300

    Refactor: refactor of core files, reuse code

---
+++ constants.js
@@ -0,0 +1,9 @@
+// boilerplate slash hardcoded stuff in its own place
+const links = {
+    discord: `https://discord.gg/rgDwAaN thedap17Nice`,
+    youtube: `https://www.youtube.com/channel/UC3CshKcydQEdd2zMfd9UsAg thedap17Ayy`,
+    github: `https://github.com/theDapperFoxtrot thedap17Ayy`,
+    website: `https://dapperfans.com/ thedap17Ayy`
+}
+
+export default links
--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-import tmi from "tmi.js";
-import dotenv from "dotenv";
-
-dotenv.config();
+import client from "./tmiConfig.js"
+import links from './constants.js'
 //Provide tmi my twitch info for access
-const client = new tmi.Client({
-  options: { debug: true },
-  identity: {
-    username: "thedapperfoxtrot",
-    password: process.env.TOKEN,
-  },
-  channels: ["thedapperfoxtrot"],
-});
+const regExp = new RegExp(/^h(?:ello(?: there)?|[aiu]llo)$/)
+const triggers = ['discord', 'youtube', 'github', 'website']
 //Connect to chat
 client.connect();
 //Watch forand log message events
@@ -19,11 +11,7 @@ client.on("message", (channel, tags, message, self) => {
   const msg = message.toLowerCase();
   if (self) return;
   //If the chatter says "hello or hello there" at any point in there message, give a classic Star Wars fan response <3
-  if (
-    msg.indexOf("hello") >= 0 ||
-    msg.indexOf("hello there") >= 0 ||
-    msg.indexOf("hullo") >= 0
-  ) {
+  if (regExp.test(msg)) {
     client.say(
       channel,
       `General @${tags.username} ...you are a bold one. thedap17Int`,
@@ -34,26 +22,20 @@ client.on("message", (channel, tags, message, self) => {
     client.say(channel, `@${tags.username}, sup dood! thedap17Ayy`);
   }
   //If the chatter says something that contains the word echo, then say that same thing right back. WHOA!
-  if (msg.indexOf("echo") >= 0) {
+  if (msg.includes("echo")) {
     client.say(channel, `@${tags.username}, ${message} thedap17Whoa`);
   }
-  //If the chatter wants to see my discord, they can get the link with discord
-  if (msg.indexOf("discord") >= 0) {
-    client.say(channel, `https://discord.gg/rgDwAaN thedap17Nice`);
-  }
-  //If the chatter mentions YouTube, then I will let them know I'm on YouTube!
-  if (msg.indexOf("youtube") >= 0) {
-    client.say(
-      channel,
-      `https://www.youtube.com/channel/UC3CshKcydQEdd2zMfd9UsAg thedap17Ayy`,
-    );
-  }
-  //If the chatter mentions GitHub, I will link them to my repos!
-  if (msg.indexOf("github") >= 0) {
-    client.say(channel, `https://github.com/theDapperFoxtrot thedap17Ayy`);
-  }
 
-  if (msg.match(/\bsite\b/gi) != null || msg.indexOf("website") >= 0) {
-    client.say(channel, `https://dapperfans.com/ thedap17Ayy`);
-  }
+  triggers.some(trigger => {
+    if(msg.includes(trigger)) {
+      client.say(channel, links[trigger])
+      return true; 
+      // return true to stop executing after first match
+      // so u dont send a ton of messages if user types 
+      // all trigger words 
+      // if necessary - remove <return true>
+    }
+  })
 });
+
+
