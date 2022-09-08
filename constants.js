244c5d41c0cb654f9301b4942d5a26838296327d
Viktor Hrytsai <vhrytsai@atlassian.com>
2022-09-08 23:00:21 +0300

    Refactor: add smiles, and custom responder

--- constants.js
+++ constants.js
@@ -1,9 +1,16 @@
 // boilerplate slash hardcoded stuff in its own place
-const links = {
-    discord: `https://discord.gg/rgDwAaN thedap17Nice`,
-    youtube: `https://www.youtube.com/channel/UC3CshKcydQEdd2zMfd9UsAg thedap17Ayy`,
-    github: `https://github.com/theDapperFoxtrot thedap17Ayy`,
-    website: `https://dapperfans.com/ thedap17Ayy`
+const smiles = {
+    nice: 'thedap17Nice',
+    ayy: 'thedap17Ayy',
+    int: 'thedap17Int',
+    whoa: 'thedap17Whoa'
+}
+const constants = {
+    discord: `https://discord.gg/rgDwAaN ${smiles.nice}`,
+    youtube: `https://www.youtube.com/channel/UC3CshKcydQEdd2zMfd9UsAg ${smiles.ayy}`,
+    github: `https://github.com/theDapperFoxtrot ${smiles.ayy}`,
+    website: `https://dapperfans.com/ ${smiles.ayy}`
 }
 
-export default links
+export { constants as constants }
+export { smiles as smiles }
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import client from "./tmiConfig.js"
-import links from './constants.js'
+import {constants} from './constants.js'
+import { respondInChat } from "./responder.js"
 //Provide tmi my twitch info for access
 const regExp = new RegExp(/^h(?:ello(?: there)?|[aiu]llo)$/)
 const triggers = ['discord', 'youtube', 'github', 'website']
@@ -12,30 +13,27 @@ client.on("message", (channel, tags, message, self) => {
   if (self) return;
   //If the chatter says "hello or hello there" at any point in there message, give a classic Star Wars fan response <3
   if (regExp.test(msg)) {
-    client.say(
-      channel,
-      `General @${tags.username} ...you are a bold one. thedap17Int`,
-    );
+    respondInChat(channel, 'hello', tags)
   }
   //If the chatter says "sup" then reciprocate and add a finger gun for good measure
-  if (msg.match(/\bsup\b/gi) != null) {
-    client.say(channel, `@${tags.username}, sup dood! thedap17Ayy`);
+  if (msg.match(/\bsup\b/gi)) {
+    respondInChat(channel, 'sup', tags)
   }
   //If the chatter says something that contains the word echo, then say that same thing right back. WHOA!
-  if (msg.includes("echo")) {
-    client.say(channel, `@${tags.username}, ${message} thedap17Whoa`);
+  if (msg.includes('echo')) {
+    respondInChat(channel, 'echo', tags, message)
   }
 
   triggers.some(trigger => {
     if(msg.includes(trigger)) {
-      client.say(channel, links[trigger])
+      client.say(channel, constants[trigger])
       return true; 
-      // return true to stop executing after first match
-      // so u dont send a ton of messages if user types 
-      // all trigger words 
-      // if necessary - remove <return true>
     }
   })
+  // return true to stop executing after first match
+  // so u dont send a ton of messages if user types 
+  // all trigger words 
+  // if necessary - remove <return true>
 });
 
 
