582933e692d6ca3cf75f1c2b5a57dcff45160488
theDapperFoxtrot <60763515+theDapperFoxtrot@users.noreply.github.com>
2022-09-13 11:57:51 -0400

    missing extension (crash fix)

--- responder.js
+++ responder.js
@@ -1,22 +1,21 @@
-import { smiles } from "./constants";
+import { smiles } from "./constants.js";
 
 const respondInChat = (channel, onMessage, tags, message = {}) => {
-    let response;
-    switch (onMessage) {
-      case 'hello':
-        response = `General @${tags.username} ...you are a bold one. ${smiles.int}`;
-        break;
-      case 'sup':
-        response = `@${tags.username}, sup dood! ${smiles.ayy}`;
-        break;
-      case 'echo':
-        response = `@${tags.username}, ${message} ${smiles.whoa}`;
-        break;
-      default:
-        break;
-    }
-    client.say(channel, response)
-}
+  let response;
+  switch (onMessage) {
+    case "hello":
+      response = `General @${tags.username} ...you are a bold one. ${smiles.int}`;
+      break;
+    case "sup":
+      response = `@${tags.username}, sup dood! ${smiles.ayy}`;
+      break;
+    case "echo":
+      response = `@${tags.username}, ${message} ${smiles.whoa}`;
+      break;
+    default:
+      break;
+  }
+  client.say(channel, response);
+};
 
-export {respondInChat}
-
+export { respondInChat };
