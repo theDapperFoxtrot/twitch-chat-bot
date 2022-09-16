a0ee001a402624a4c241319e5b5e37d463fa156a
theDapperFoxtrot <60763515+theDapperFoxtrot@users.noreply.github.com>
2022-09-16 18:04:28 -0400

    added chat commands

--- constants.js
+++ constants.js
@@ -1,16 +1,18 @@
 // boilerplate slash hardcoded stuff in its own place
 const smiles = {
-    nice: 'thedap17Nice',
-    ayy: 'thedap17Ayy',
-    int: 'thedap17Int',
-    whoa: 'thedap17Whoa'
-}
+  nice: "thedap17Nice",
+  ayy: "thedap17Ayy",
+  int: "thedap17Int",
+  whoa: "thedap17Whoa",
+};
 const constants = {
-    discord: `https://discord.gg/rgDwAaN ${smiles.nice}`,
-    youtube: `https://www.youtube.com/channel/UC3CshKcydQEdd2zMfd9UsAg ${smiles.ayy}`,
-    github: `https://github.com/theDapperFoxtrot ${smiles.ayy}`,
-    website: `https://dapperfans.com/ ${smiles.ayy}`
-}
+  discord: `https://discord.gg/rgDwAaN ${smiles.nice}`,
+  youtube: `https://www.youtube.com/channel/UC3CshKcydQEdd2zMfd9UsAg ${smiles.ayy}`,
+  github: `https://github.com/theDapperFoxtrot ${smiles.ayy}`,
+  website: `https://dapperfans.com/ ${smiles.ayy}`,
+  "!today": `We're going to be working on a login form to create users ${smiles.nice}`,
+  "!project": `We're trying to improve the Dapper Twitch experience with a chatbot that works in chat and on DapperFans.com`,
+};
 
-export { constants as constants }
-export { smiles as smiles }
+export { constants as constants };
+export { smiles as smiles };
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import client from "./tmiConfig.js"
-import {constants} from './constants.js'
-import { respondInChat } from "./responder.js"
+import client from "./tmiConfig.js";
+import { constants } from "./constants.js";
+import { respondInChat } from "./responder.js";
 //Provide tmi my twitch info for access
-const regExp = new RegExp(/^h(?:ello(?: there)?|[aiu]llo)$/)
-const triggers = ['discord', 'youtube', 'github', 'website']
+const regExp = new RegExp(/^h(?:ello(?: there)?|[aiu]llo)$/);
+const triggers = ["discord", "youtube", "github", "website", "!today", "!project"];
 //Connect to chat
 client.connect();
 //Watch forand log message events
@@ -13,27 +13,25 @@ client.on("message", (channel, tags, message, self) => {
   if (self) return;
   //If the chatter says "hello or hello there" at any point in there message, give a classic Star Wars fan response <3
   if (regExp.test(msg)) {
-    respondInChat(channel, 'hello', tags)
+    respondInChat(channel, "hello", tags);
   }
   //If the chatter says "sup" then reciprocate and add a finger gun for good measure
   if (msg.match(/\bsup\b/gi)) {
-    respondInChat(channel, 'sup', tags)
+    respondInChat(channel, "sup", tags);
   }
   //If the chatter says something that contains the word echo, then say that same thing right back. WHOA!
-  if (msg.includes('echo')) {
-    respondInChat(channel, 'echo', tags, message)
+  if (msg.includes("echo")) {
+    respondInChat(channel, "echo", tags, message);
   }
 
-  triggers.some(trigger => {
-    if(msg.includes(trigger)) {
-      client.say(channel, constants[trigger])
-      return true; 
+  triggers.some((trigger) => {
+    if (msg.includes(trigger)) {
+      client.say(channel, constants[trigger]);
+      return true;
     }
-  })
+  });
   // return true to stop executing after first match
-  // so u dont send a ton of messages if user types 
-  // all trigger words 
+  // so u dont send a ton of messages if user types
+  // all trigger words
   // if necessary - remove <return true>
 });
-
-
