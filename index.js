d41761bfb3cb7999468567972e37b034af7279b4
theDapperFoxtrot <60763515+theDapperFoxtrot@users.noreply.github.com>
2023-09-08 13:44:29 +0300

    refactored to personal preference

--- index.js
+++ index.js
@@ -1,29 +1,75 @@
+// import tmi settings
 import client from "./tmiConfig.js";
 
-// constants
+//Connect to chat
+client.connect();
+
+client.on("message", (channel, tags, message, self) => {
+  //All messages are made lowercase for easy scanning.
+  const msg = message.toLowerCase();
+
+  //Watch for and log message events
+  if (self) return;
+
+  // GREETINGS
+  //If the chatter says "hello or hello there" at any point in there message, give a classic Star Wars fan response <3
+  if (helloExpression.test(msg)) {
+    respondInChat(channel, "hello", tags);
+  }
+  //If the chatter says "sup" then reciprocate and add a finger gun for good measure
+  if (msg.match(/\bsup\b/gi)) {
+    respondInChat(channel, "sup", tags);
+  }
+  //If the chatter says something that contains the word echo, then say that same thing right back. WHOA!
+
+  // SOCIALS/SITES
+  if (msg.includes("facebook")) {
+    respondInChat(channel, "facebook", tags, message);
+  }
+  if (msg.includes("github")) {
+    respondInChat(channel, "github", tags, message);
+  }
+  if (msg.includes("insta")) {
+    respondInChat(channel, "instagram", tags, message);
+  }
+  if (msg.includes("twitter")) {
+    respondInChat(channel, "twitter", tags, message);
+  }
+  if (msg.includes("website")) {
+    respondInChat(channel, "website", tags, message);
+  }
+  if (msg.includes("youtube")) {
+    respondInChat(channel, "youtube", tags, message);
+  }
+
+  // CHAT THINGS
+  // To annouce you are lurking
+  if (msg.includes("!lurk")) {
+    respondInChat(channel, "!lurk", tags);
+  }
+  // To annouce your return from lurking
+  if (msg.includes("!unlurk")) {
+    respondInChat(channel, "!unlurk", tags);
+  }
+
+  // FUN STUFF
+  if (msg.includes("echo")) {
+    respondInChat(channel, "echo", tags, message);
+  }
+});
+
+
+// twitch emote codes
 const smiles = {
   nice: "thedap17Nice",
   ayy: "thedap17Ayy",
   int: "thedap17Int",
   whoa: "thedap17Whoa",
 };
-const constants = {
-  // discord: `https://discord.gg/rgDwAaN ${smiles.nice}`,
-  youtube: `https://www.youtube.com/@theDapperFoxtrot ${smiles.ayy}`,
-  github: `https://github.com/theDapperFoxtrot ${smiles.ayy}`,
-  website: `https://dapperfans.com/ ${smiles.ayy}`,
-  "!today": `Baldur's Gate 3, what else?! ${smiles.nice}`,
-  "!project": `Nothing on the go...yet!`,
-};
 
-//Provide tmi my twitch info for access
-const regExp = new RegExp(/^h(?:ello(?: there)?|[aiu]llo)$/);
-const triggers = [
-  // "discord",
-  "!lurk", "!unlurk", "youtube", "github", "website", "!today", "!project"];
-//Connect to chat
-client.connect();
-//Watch for and log message events
+// Catching variations of spelling "hello"
+const helloExpression = new RegExp(/^h(?:ello(?: there)?|[aiu]llo)$/);
+
 
 const respondInChat = (channel, onMessage, tags, message = {}) => {
   let response;
@@ -32,16 +78,37 @@ const respondInChat = (channel, onMessage, tags, message = {}) => {
       response = `General @${tags.username} ...you are a bold one. ${smiles.int}`;
       break;
     case "sup":
-      response = `@${tags.username}, suh dood! ${smiles.ayy}`;
+      response = `@${tags.username}, sup dood! ${smiles.ayy}`;
       break;
-    case "echo":
-      response = `@${tags.username}, ${message} ${smiles.whoa}`;
+    case "facebook":
+      response = `There is a Facebook page if you're into that https://www.facebook.com/gaming/DapperFoxtrot ${smiles.ayy}`;
+      break;
+    case "instagram":
+      response = `Did someone mention insta? https://www.instagram.com/thedapperfoxtrot/ ${smiles.ayy}`;
+      break;
+    case "youtube":
+      response = `https://www.youtube.com/@theDapperFoxtrot ${smiles.ayy}`;
+      break;
+    case "github":
+      response = `https://github.com/theDapperFoxtrot ${smiles.ayy}`;
+      break;
+    case "twitter":
+      response = `https://twitter.com/FoxtrotDapper ${smiles.ayy}`;
+      break;
+    case "website":
+      response = `https://dapperfans.com/ ${smiles.ayy}`;
       break;
     case "!lurk":
       response = `Welcome to the lurk zone, @${tags.username}! Get cozy, we'll talk to you soon! ${smiles.nice}`;
       break;
     case "!unlurk":
       response = `Welcome back, @${tags.username}! ${smiles.ayy}`;
+      break;
+    case "echo":
+      response = `@${tags.username}, ${message} ${smiles.whoa}`;
+      break;
+    case "!commands":
+      response = `Find them here: https://dapperfans.com/chat-commands @${tags.username}! ${smiles.ayy}`;
       break;
 
     default:
@@ -50,39 +117,3 @@ const respondInChat = (channel, onMessage, tags, message = {}) => {
   client.say(channel, response);
 };
 
-client.on("message", (channel, tags, message, self) => {
-  //All messages are made lowercase for easy scanning.
-  const msg = message.toLowerCase();
-  if (self) return;
-  //If the chatter says "hello or hello there" at any point in there message, give a classic Star Wars fan response <3
-  if (regExp.test(msg)) {
-    respondInChat(channel, "hello", tags);
-  }
-  //If the chatter says "sup" then reciprocate and add a finger gun for good measure
-  if (msg.match(/\bsup\b/gi)) {
-    respondInChat(channel, "sup", tags);
-  }
-  //If the chatter says something that contains the word echo, then say that same thing right back. WHOA!
-  if (msg.includes("echo")) {
-    respondInChat(channel, "echo", tags, message);
-  }
-  // // To annouce you are lurking
-  // if (msg.includes("!lurk")) {
-  //   respondInChat(channel, "!lurk", tags);
-  // }
-  // // To annouce your return from lurking
-  // if (msg.includes("!unlurk")) {
-  //   respondInChat(channel, "!unlurk", tags);
-  // }
-
-  triggers.some((trigger) => {
-    if (msg.includes(trigger)) {
-      client.say(channel, constants[trigger]);
-      return true;
-    }
-  });
-  // return true to stop executing after first match
-  // so u dont send a ton of messages if user types
-  // all trigger words
-  // if necessary - remove <return true>
-});
