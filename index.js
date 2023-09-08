4a0b5a89d55a4666bb1564a93b0b506466a35034
theDapperFoxtrot <60763515+theDapperFoxtrot@users.noreply.github.com>
2023-09-08 09:19:02 +0300

    consolidate
    Yes, it's not as organized, but as a personal project, I like having it all in one file. Navigation in VS code is simplified for me. I can always refactor again later.

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 import client from "./tmiConfig.js";
-import { constants } from "./constants.js";
-import { respondInChat } from "./responder.js";
+
+// constants
+const smiles = {
+  nice: "thedap17Nice",
+  ayy: "thedap17Ayy",
+  int: "thedap17Int",
+  whoa: "thedap17Whoa",
+};
+const constants = {
+  // discord: `https://discord.gg/rgDwAaN ${smiles.nice}`,
+  youtube: `https://www.youtube.com/@theDapperFoxtrot ${smiles.ayy}`,
+  github: `https://github.com/theDapperFoxtrot ${smiles.ayy}`,
+  website: `https://dapperfans.com/ ${smiles.ayy}`,
+  "!today": `Baldur's Gate 3, what else?! ${smiles.nice}`,
+  "!project": `Nothing on the go...yet!`,
+};
+
 //Provide tmi my twitch info for access
 const regExp = new RegExp(/^h(?:ello(?: there)?|[aiu]llo)$/);
 const triggers = [
@@ -8,7 +23,33 @@ const triggers = [
   "!lurk", "!unlurk", "youtube", "github", "website", "!today", "!project"];
 //Connect to chat
 client.connect();
-//Watch forand log message events
+//Watch for and log message events
+
+const respondInChat = (channel, onMessage, tags, message = {}) => {
+  let response;
+  switch (onMessage) {
+    case "hello":
+      response = `General @${tags.username} ...you are a bold one. ${smiles.int}`;
+      break;
+    case "sup":
+      response = `@${tags.username}, suh dood! ${smiles.ayy}`;
+      break;
+    case "echo":
+      response = `@${tags.username}, ${message} ${smiles.whoa}`;
+      break;
+    case "!lurk":
+      response = `Welcome to the lurk zone, @${tags.username}! Get cozy, we'll talk to you soon! ${smiles.nice}`;
+      break;
+    case "!unlurk":
+      response = `Welcome back, @${tags.username}! ${smiles.ayy}`;
+      break;
+
+    default:
+      break;
+  }
+  client.say(channel, response);
+};
+
 client.on("message", (channel, tags, message, self) => {
   //All messages are made lowercase for easy scanning.
   const msg = message.toLowerCase();
@@ -25,14 +66,14 @@ client.on("message", (channel, tags, message, self) => {
   if (msg.includes("echo")) {
     respondInChat(channel, "echo", tags, message);
   }
-  // To annouce you are lurking
-  if (msg.includes("!lurk")) {
-    respondInChat(channel, "!lurk", tags);
-  }
-  // To annouce your return from lurking
-  if (msg.includes("!unlurk")) {
-    respondInChat(channel, "!unlurk", tags);
-  }
+  // // To annouce you are lurking
+  // if (msg.includes("!lurk")) {
+  //   respondInChat(channel, "!lurk", tags);
+  // }
+  // // To annouce your return from lurking
+  // if (msg.includes("!unlurk")) {
+  //   respondInChat(channel, "!unlurk", tags);
+  // }
 
   triggers.some((trigger) => {
     if (msg.includes(trigger)) {
